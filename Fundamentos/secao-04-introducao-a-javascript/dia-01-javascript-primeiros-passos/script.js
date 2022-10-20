e226db60f2d475191e61618ad44fbd5e7ff0bfb8
Guilherme Lemos <guilhermelemospimentel@gmail.com>
2022-10-20 20:18:06 -0300

    fazendo o exercicio de js mais desafiador ate agora

--- Fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -18,8 +18,23 @@
 
 //exercicio 3
 
-let number;
+//let number;
 
-if (number > 0) {console.log ('positive');}
-else if (number < 0) {console.log ('negative');}
-else {console.log ('zero')}
+//if (number > 0) {console.log ('positive');}
+//else if (number < 0) {console.log ('negative');}
+//else {console.log ('zero')}
+
+//exercicio 4
+
+const a = 30;
+const b = 75;
+const c = -20;
+
+let somaDasConst = a+b+c;
+let allAnglesArePositive = a > 0 && b > 0 && c > 0;
+
+if (allAnglesArePositive){
+if (somaDasConst === 180) {console.log (true);}
+else {console.log (false);}
+}
+else {console.log ('erro - ang invalido')};
