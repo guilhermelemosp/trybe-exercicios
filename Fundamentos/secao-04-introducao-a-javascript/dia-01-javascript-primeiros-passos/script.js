7c23db3d7aca08ad63ac61c550a6715e20bd4065
Guilherme Lemos <guilhermelemospimentel@gmail.com>
2022-10-20 19:48:15 -0300

    resolucao do exercicio 3

--- Fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -4,10 +4,22 @@
 //if (a > b) {console.log ('a é maior que b');}
 //else {console.log('b é maior que a')};//
 
-const a = 123;
-const b = 321;
-const c = 20;
 
-if (a > b && a > c ) {console.log ('o maior numero é: ' + a + ' (a)')}
-else if (b > a && b > c ) {console.log ('o maior numero é: ' + b + ' (b)')}
-else if (c > a && c > b ) {console.log ('o maior numero é: ' + c + ' (c)')}
+
+//const a = 123;
+//const b = 321;
+//const c = 20;
+
+//if (a > b && a > c ) {console.log ('o maior numero é: ' + a + ' (a)')}
+//else if (b > a && b > c ) {console.log ('o maior numero é: ' + b + ' (b)')}
+//else if (c > a && c > b ) {console.log ('o maior numero é: ' + c + ' (c)')}
+
+
+
+//exercicio 3
+
+let number;
+
+if (number > 0) {console.log ('positive');}
+else if (number < 0) {console.log ('negative');}
+else {console.log ('zero')}
---
+++ Fundamentos/secao-04-introducao-a-javascript/script.js
@@ -0,0 +1,5 @@
+const currentHour = 4
+
+let message;
+
+if (currentHour >= 22) {message = “Não deveríamos comer nada, é hora de dormir”}
