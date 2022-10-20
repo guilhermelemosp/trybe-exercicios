52d30567c862a7f61105248e7fda034c94a50706
Guilherme Lemos <guilhermelemospimentel@gmail.com>
2022-10-20 19:33:34 -0300

    atividade 2

---
+++ Fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,13 @@
+//const a = 10;
+//const b = 5;
+
+//if (a > b) {console.log ('a é maior que b');}
+//else {console.log('b é maior que a')};//
+
+const a = 123;
+const b = 321;
+const c = 20;
+
+if (a > b && a > c ) {console.log ('o maior numero é: ' + a + ' (a)')}
+else if (b > a && b > c ) {console.log ('o maior numero é: ' + b + ' (b)')}
+else if (c > a && c > b ) {console.log ('o maior numero é: ' + c + ' (c)')}
