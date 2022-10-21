b19a4d0ce6a8f9c3574bbc7cf18c372d219e6a7c
Guilherme Lemos <guilhermelemospimentel@gmail.com>
2022-10-21 20:33:24 -0300

    praticando exercícios de for

---
+++ Fundamentos/secao-04-introducao-a-javascript/dia-02-js-array-e-loop-for/estruturaderepeticao.js
@@ -0,0 +1,31 @@
+
+
+//for (expressão inicial; condição a ser cumprida; atualização da expressão inicial)
+
+//let anonovo = 0
+
+//for (let index = 10; index >= 1; index = index -1) {
+  //  console.log (index)}
+
+//console.log ('FELIZ ANO NOVO UHULLLLLLL')
+
+//const listaDeCompras = [
+    //'maçã',
+    //'gengibre',
+    //'miojo',
+    //'biscoito/bolacha',
+    //'banana',
+    //'feijão carioquinha',
+    //'pasta de dente',
+  //]; // itens para comprar
+  //const carrinho = []; // array vazio que irá receber os itens da variável listaDeCompras
+
+  //for (let index=0; index < listaDeCompras.length ; index = index +1) {
+//if (listaDeCompras[index] === 'maçã' || listaDeCompras[index] === 'banana') {carrinho.push (listaDeCompras[index])}
+  //}
+
+  //console.log (carrinho)
+
+  let numero = 9;
+
+  for (let index = 1 ; index <= 10 ; index = index + 1) {console.log (numero * index)}
