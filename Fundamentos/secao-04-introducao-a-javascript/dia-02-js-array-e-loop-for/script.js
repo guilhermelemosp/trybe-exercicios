02fc7f1835cf16912d107e8e49d2a816f824e8a7
Guilherme Lemos <guilhermelemospimentel@gmail.com>
2022-10-21 18:32:59 -0300

    resolvendo exercicios de JS

---
+++ Fundamentos/secao-04-introducao-a-javascript/dia-02-js-array-e-loop-for/script.js
@@ -0,0 +1,31 @@
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let menuServices = menu [1];
+
+//console.log(menuServices);
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let indexOfPortfolio = menu.indexOf('Portfólio');
+
+//console.log(indexOfPortfolio);
+
+
+//const convidados = ['Adilson', 'Claudio', 'Juliana', 'Vinícius'];
+
+//convidados [2] = 'Laura'
+//convidados.push ('Guilherme', 'Letícia')
+
+//console.log (convidados)
+
+//let saboresSorvete = ['Chocolate', 'Morango', 'Açaí', 'Creme', 'Flocos'];
+//let cobertura = ['Chocolate', 'Morango', 'Doce de Leite']
+
+//console.log ('Quantidade de sabores de sorvete: ' + saboresSorvete.length);
+//console.log ('Quantidade de sabores de cobertura: ' + cobertura.length)
+
+
+let lista = [1, 2, 3, 4, 5];
+let inverter = true;
+let listaInvertida = [];
+
+if (inverter = true) {listaInvertida.push = [5, 4, 3, 2, 1]}
+else {console.log (lista)}
