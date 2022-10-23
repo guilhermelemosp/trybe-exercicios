8468e20b556aa88ccb5dc2b6118badb850df720a
Guilherme Lemos <guilhermelemospimentel@gmail.com>
2022-10-23 19:48:12 -0300

    exercicio de numeros impares

--- Fundamentos/secao-04-introducao-a-javascript/dia-02-js-array-e-loop-for/exercicios-modulo-dia-02-js.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-02-js-array-e-loop-for/exercicios-modulo-dia-02-js.js
@@ -37,18 +37,6 @@ console.log (media)
 
 *********Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”; **********
 
-
-
-Utilizando for, descubra qual o maior valor contido no array e imprima-o;
-
-Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
-
-Utilizando for, descubra qual o menor valor contido no array e imprima-o;
-
-Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
-
-Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.*/
-
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 var media = 0
 
@@ -57,9 +45,46 @@ for (index = 0 ; index < numbers.length ; index++) {
 media += numbers[index]/numbers.length
 
 }
-
 console.log (media)
-
 
 if (media > 20) {console.log ('valor maior que 20')}
 else {console.log ('valor menor que 20')}
+
+
+************* Utilizando for, descubra qual o maior valor contido no array e imprima-o;************
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let higherNumber = 0
+
+for ( index = 0 ; index < numbers.length ; index++) {
+if (higherNumber < numbers[index]) {
+
+    higherNumber = numbers[index]
+}
+
+}
+
+console.log (higherNumber)
+
+***************** Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”; *****************
+
+************* Utilizando for, descubra qual o menor valor contido no array e imprima-o;************
+
+************Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;*****************
+
+********** Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.***********/
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let quantidadeDeImpares = 0
+
+for ( index = 0 ; index < numbers.length ; index++) {
+if ( numbers[index] % 2 !== 0) {
+    quantidadeDeImpares += 1
+}
+}
+
+if (quantidadeDeImpares === 0) {
+    console.log ('Nenhum valor impar encontrado')
+}
+else {console.log ('numeros impares encontrados: ' + quantidadeDeImpares)}
