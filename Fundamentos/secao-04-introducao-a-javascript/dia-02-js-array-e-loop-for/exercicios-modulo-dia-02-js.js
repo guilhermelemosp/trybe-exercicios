cc71ffceb13a84bf39a638b93d8015a7e7a51d9e
Guilherme Lemos <guilhermelemospimentel@gmail.com>
2022-10-23 20:11:01 -0300

    salvando alteraçoes

--- Fundamentos/secao-04-introducao-a-javascript/dia-02-js-array-e-loop-for/exercicios-modulo-dia-02-js.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-02-js-array-e-loop-for/exercicios-modulo-dia-02-js.js
@@ -113,7 +113,6 @@ console.log (numbers)
 
 
 ********** Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.***********/
-
 let numbers = [
     0,  1,  2,  3,  4,  5,  6,  7,
     8,  9, 10, 11, 12, 13, 14, 15,
@@ -127,3 +126,4 @@ for (let index = 0 ; index < numbers.length ; index++) {
     console.log (numbers[index] / 2)
 
 }
+
