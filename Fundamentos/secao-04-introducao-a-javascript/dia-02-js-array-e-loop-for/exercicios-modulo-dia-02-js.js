f3ae4b49b39bca27a829f3df6feb73bd8028340c
Guilherme Lemos <guilhermelemospimentel@gmail.com>
2022-10-23 18:56:13 -0300

    resolucao de exercicios

--- Fundamentos/secao-04-introducao-a-javascript/dia-02-js-array-e-loop-for/exercicios-modulo-dia-02-js.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-02-js-array-e-loop-for/exercicios-modulo-dia-02-js.js
@@ -1,15 +1,43 @@
-/*Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+/*
 
-Some todos os valores contidos no array e imprima o resultado;
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+****** Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; ******
 
 console.log (numbers);
 
 
-Calcule e imprima a média aritmética dos valores contidos no array;
+******* Some todos os valores contidos no array e imprima o resultado; *******
 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+var soma = 0
+
+for (index = 0 ; index < numbers.length ; index++) {
+
+soma += numbers[index] 
+}
+
+console.log (soma)
+
+*************Calcule e imprima a média aritmética dos valores contidos no array;******************
 A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
-Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+var media = 0
+
+for (index = 0 ; index < numbers.length ; index++) {
+
+media += numbers[index]/numbers.length
+
+if (numbers < )
+}
+
+console.log (media)
+
+
+
+*********Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”; **********
+
+
 
 Utilizando for, descubra qual o maior valor contido no array e imprima-o;
 
@@ -22,5 +50,16 @@ Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
 Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.*/
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+var media = 0
 
-console.log (numbers);
+for (index = 0 ; index < numbers.length ; index++) {
+
+media += numbers[index]/numbers.length
+
+}
+
+console.log (media)
+
+
+if (media > 20) {console.log ('valor maior que 20')}
+else {console.log ('valor menor que 20')}
