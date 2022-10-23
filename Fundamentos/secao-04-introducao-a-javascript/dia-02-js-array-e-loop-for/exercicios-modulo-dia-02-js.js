390ae24bcabd8725de7718140adac08ead231552
Guilherme Lemos <guilhermelemospimentel@gmail.com>
2022-10-23 20:09:24 -0300

    finalizando exercicios de array e loop

--- Fundamentos/secao-04-introducao-a-javascript/dia-02-js-array-e-loop-for/exercicios-modulo-dia-02-js.js
+++ Fundamentos/secao-04-introducao-a-javascript/dia-02-js-array-e-loop-for/exercicios-modulo-dia-02-js.js
@@ -68,12 +68,6 @@ console.log (higherNumber)
 
 ***************** Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”; *****************
 
-************* Utilizando for, descubra qual o menor valor contido no array e imprima-o;************
-
-************Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;*****************
-
-********** Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.***********/
-
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let quantidadeDeImpares = 0
@@ -88,3 +82,48 @@ if (quantidadeDeImpares === 0) {
     console.log ('Nenhum valor impar encontrado')
 }
 else {console.log ('numeros impares encontrados: ' + quantidadeDeImpares)}
+
+
+
+************* Utilizando for, descubra qual o menor valor contido no array e imprima-o;************
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let smallerNumber = numbers [0] //o numero 0 indica o valor do primeiro numero da array, no caso 5. Estou buscando o menor valor a partir do primeiro numero da array, que nesse caso esta no index 0.
+
+for ( index = 1 ; index < numbers.length ; index++) {
+if (smallerNumber > numbers[index]) {
+
+    smallerNumber = numbers[index]
+}
+
+}
+
+console.log (smallerNumber)
+
+************Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;*****************
+
+let numbers = [];
+
+
+for ( index = 0 ; index <= 25 ; index++) {
+numbers.push (index)}
+
+console.log (numbers)
+
+
+********** Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.***********/
+
+let numbers = [
+    0,  1,  2,  3,  4,  5,  6,  7,
+    8,  9, 10, 11, 12, 13, 14, 15,
+   16, 17, 18, 19, 20, 21, 22, 23,
+   24, 25
+ ]
+ 
+
+
+for (let index = 0 ; index < numbers.length ; index++) {
+    console.log (numbers[index] / 2)
+
+}
