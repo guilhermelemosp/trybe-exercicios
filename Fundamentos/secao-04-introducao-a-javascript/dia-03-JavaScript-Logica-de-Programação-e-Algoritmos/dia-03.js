462b4f5cbd275b0aee64b000ed6fed5693d0269f
Guilherme Lemos <guilhermelemospimentel@gmail.com>
2022-10-24 16:12:49 -0300

    exercicios anteriores a aula 03-js

---
+++ Fundamentos/secao-04-introducao-a-javascript/dia-03-JavaScript-Logica-de-Programação-e-Algoritmos/dia-03.js
@@ -0,0 +1,29 @@
+/*let fruits = [3, 4, 4, 1, 2];
+
+var soma = 0
+
+for (i=0 ; i < fruits.length ; i++) {
+    soma += fruits[i]
+  
+}
+
+if (soma > 15) {console.log (soma)}
+else {console.log ('valor menor que 15')}*/
+
+/*const n = 9 ;
+
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);*/
+
+/*let soma = 0
+
+for (index = 0 ; index <= 100 ; index++) {
+
+    soma += index
+}
+
+console.log (soma)*/
+
