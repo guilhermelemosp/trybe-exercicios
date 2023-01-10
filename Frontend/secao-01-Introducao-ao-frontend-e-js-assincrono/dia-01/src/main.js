176dac55e706671a39794a82fb4a9752f21bde55
Guilherme Lemos <guilhermelemospimentel@gmail.com>
2023-01-10 18:59:02 -0300

    finalizando atividade dia 01 frontend

---
+++ Frontend/secao-01-Introducao-ao-frontend-e-js-assincrono/dia-01/src/main.js
@@ -0,0 +1,22 @@
+import validator from "validator";
+
+
+const campoDeTexto = document.querySelector('#value');
+const botao = document.querySelector('#button');
+const seletor = document.querySelector('#option');
+const saidaTexto = document.querySelector('#answer');
+const UUID_VERSION = 4
+
+botao.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const campos = {
+        cpf: validator.isTaxID(campoDeTexto.value, 'pt-BR'),
+        hexColor: validator.isHexColor(campoDeTexto.value),
+        email: validator.isEmail(campoDeTexto.value),
+        uuid: validator.isUUID(campoDeTexto.value, UUID_VERSION),
+        url: validator.isURL(campoDeTexto.value),
+      };
+      saidaTexto.innerHTML = `A validação retornou ${campos[seletor.value]}`;
+
+})
